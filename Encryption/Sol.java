627fdbbecec74d4c5644f3dcf8d2d99d3e5b19c4
unknown <dimeddy97@gmail.com>
2019-09-02 21:40:43 +0300

    encryption

---
+++ Encryption/Sol.java
@@ -0,0 +1,23 @@
+public class Sol
+{
+	static String encryption(String s)
+	{
+        String pr = "";
+        s = s.replaceAll(" ", "");
+           
+        int len = s.length(), r = 0, c = 0;
+        int rows = (int)Math.floor(Math.sqrt(len));
+        int cols = (int)Math.ceil(Math.sqrt(len));
+
+        for(int i = 0;i < len;i++)
+        {
+            pr += s.charAt(r);           
+            if((r += cols) >= len)
+            {
+                r = ++c;
+                pr += " ";
+            }
+        }
+        return pr;
+    }
+}
